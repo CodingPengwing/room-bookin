daad1297b435b08c70ff450bb149fa24f95f531f
Tony Dang <tungtonydang@gmail.com>
2025-01-09 13:37:39 +0000

    Set up NextUi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import {Button} from "@nextui-org/react"
+
 export default function Home() {
-  return <div>Welcome to Room Bookin!</div>;
+  return (
+    <div>
+      <div>Welcome to Room Bookin!</div>
+      <Button>Click me</Button>
+    </div>
+  ); 
 }
