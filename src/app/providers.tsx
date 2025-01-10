ef97ae343792ef380c61a5e7541a9845f8051ca7
Tony Dang <tungtonydang@gmail.com>
2025-01-10 03:58:43 +0000

    Add Mantis sidebar and top nav

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import DashboardLayout from "@/layout";
+import ThemeCustomization from "@/themes";
 import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
+import Locales from "@/components/Locales";
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <ThemeCustomization>
+        <Locales>
+          <SessionProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+          </SessionProvider>
+        </Locales>
+      </ThemeCustomization>
+    </NextUIProvider>
+  );
 }
